1037b06c3a404ce119a9c05c2bc4e36da9e37cd4
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-07-02 10:48:57 +0000

    Deploying to gh-pages from @ OKoHop/goit-js-hw-10@371f9cab6b4c9c19ea35d152ceedab5def76093e 🚀

---
+++ index.3ee75a9f.js
@@ -0,0 +1,2 @@
+const e={closeBtn:document.querySelector(".close-btn"),modal:document.querySelector(".backdrop")};e.closeBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}));
+//# sourceMappingURL=index.3ee75a9f.js.map
---
+++ index.8e92e1a6.js
@@ -0,0 +1,2 @@
+!function(){var e={closeBtn:document.querySelector(".close-btn"),modal:document.querySelector(".backdrop")};e.closeBtn.addEventListener("click",(function(){e.modal.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.8e92e1a6.js.map
